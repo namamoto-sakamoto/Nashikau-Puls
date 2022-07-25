6dba53b1b024b089290d570fb68e260c54af36fe
namamoto <emaraku1009@gmail.com>
2022-07-25 13:36:26 +0000

    react削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,6 +21,3 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 require("jquery")// Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
