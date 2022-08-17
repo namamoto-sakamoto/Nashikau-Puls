adf96e5e6849dd271db996865ee81b567c657afc
namamoto <emaraku1009@gmail.com>
2022-08-17 17:03:57 +0000

    レイアウト調整

--- app/models/jpear.rb
+++ app/models/jpear.rb
@@ -8,20 +8,12 @@ class Jpear < ApplicationRecord
   has_one_attached :image
   def get_image(width, height)
     unless image.attached?
-      file_path = Rails.root.join('app/assets/images/nashihana.jpg')
+      file_path = Rails.root.join('app/assets/images/fruit_nashi_pear.png')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image.variant(resize_to_limit: [width, height]).processed
   end
   
-  # def get_image(width, height)
-  #   unless image.attached?
-  #     file_path = Rails.root.join('app/assets/images/no_image.jpg')
-  #     image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-  #   end
-  #   image.variant(resize_to_limit: [width, height]).processed
-  # end
-
   #販売状況と文字列の紐づけ
   enum status: { sold: 0, sale: 1 }
 end
