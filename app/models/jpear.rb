d77afb4eeb96a77702c092c14589440ac33d37ab
namamoto <emaraku1009@gmail.com>
2022-08-17 10:55:09 +0000

    image未投稿時の処理

--- app/models/jpear.rb
+++ app/models/jpear.rb
@@ -7,10 +7,20 @@ class Jpear < ApplicationRecord
   
   has_one_attached :image
   def get_image(width, height)
-    if image.attached?
-      image.variant(resize_to_limit: [width, height]).processed
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/nashihana.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
+    image.variant(resize_to_limit: [width, height]).processed
   end
+  
+  # def get_image(width, height)
+  #   unless image.attached?
+  #     file_path = Rails.root.join('app/assets/images/no_image.jpg')
+  #     image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  #   end
+  #   image.variant(resize_to_limit: [width, height]).processed
+  # end
 
   #販売状況と文字列の紐づけ
   enum status: { sold: 0, sale: 1 }
